84a17287f09f8b25fc03dc842a3b397120edc395
fsfod <fsfod11@gmail.com>
2018-02-20 18:05:10 +0000

    Add support for running LuaJITs sampled based profiler when running benchmarks using the command line option --jprof

--- simplerunner.lua
+++ simplerunner.lua
@@ -224,6 +224,11 @@ function runner.load_jitstats()
     return success
 end
 
+local jprof_mode, jprof_output
+function runner.start_profiler()
+    jit_profile.start(jprof_mode, jprof_output)
+end
+
 function runner.jitfunc(f, n)
     assert(jit_util, "cannot prejit with no jit.util")
     n = n or 200
@@ -271,6 +276,10 @@ function runner.runbench(name, count, scaling)
     else
        times = {}
     end
+    if jit_profile then
+        runner.start_profiler()
+    end
+    
     io.write("Running "..name..": ")
     io.flush()
 
@@ -304,6 +313,10 @@ function runner.runbench(name, count, scaling)
     -- Force a new line after our line of dots
     io.write("\n")
     io.flush()
+    
+    if jit_profile then
+        jit_profile.stop()
+    end
 
     if jitstats then
         jitstats.stop()
@@ -482,6 +495,7 @@ Usage: simplerunner [OPTION] [<benchmark>] [<count>]
 
   --jitstats           Collect and print jit statisitcs from running the benchmarks.
   --jdump options      Run LuaJIT's jit.dump module with the specifed options.
+  --jprof options [, ourout file] Run the benchmaks under LuaJIT's sample based profiler
   --benchloadjitstats  Also collect jitstats when loading a benchmark
   --nogc               Run the benchmarks with the GC disabled.
   --norandomize        Don't randomize the run order of the benchmarks.
@@ -512,6 +526,17 @@ function opt_map.jdump(args)
     end
 end
 function opt_map.vm(args) g_opt.vm = optparam(args) end
+function opt_map.jprof(args)
+    local options = optparam(args)
+    local outfile = options:find(",")
+
+    if outfile then
+        g_opt.jprof_output = options:sub(outfile + 1)
+        g_opt.jprof = options:sub(1, outfile-1)
+    else
+        g_opt.jprof = options
+    end
+end
 
 ------------------------------------------------------------------------------
 
@@ -623,6 +648,11 @@ function runner.processoptions(options)
     if options.jdump then
         jit_dump = require("jit.dump")
         jit_dump.on(options.jdump, options.jdump_output)
+    end
+    
+    if options.jprof then
+        jit_profile = require("jit.p")
+        jprof_mode = options.jprof
     end
 end
 
