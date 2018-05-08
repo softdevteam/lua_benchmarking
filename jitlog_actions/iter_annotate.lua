0ef9078a277f8231de7082632e513f94ef460860
fsfod <fsfod11@gmail.com>
2018-05-08 14:43:25 +0100

    Extend iter_annotate and make_kruninstr jitlog actions to handle vmperf

--- jitlog_actions/iter_annotate.lua
+++ jitlog_actions/iter_annotate.lua
@@ -1,4 +1,14 @@
-local names = {
+local mixin = {
+  start_marker = "BEGIN",
+  mixins = {"markerstats"},
+  actions = {}
+}
+
+function mixin:init()
+  self.itern = 0
+end
+
+mixin.statfields = {
   "traces",
   "aborts",
   "flushes",
@@ -13,7 +23,7 @@ local tscfreq = 3700000000.0
 
 local function getdiff(b, e)
   local t = {}
-  for _, k in ipairs(names) do
+  for _, k in ipairs(mixin.statfields) do
     local val = e[k]
     assert(val)
     if type(val) == "number" then
@@ -31,38 +41,96 @@ local function getdiff(b, e)
   return t
 end
 
-local mixin = {
-  mixins = {"markerstats"},
-  init = function(self)
-    self.itern = 0
-  end,
-  actions = {
-    stringmarker = function(self, msg, marker)
-      if marker.label == "BEGIN" then
-        self.mstart = marker
-        self.mend = nil
-        marker.n = self.itern
-        self.itern = self.itern + 1
-      elseif marker.label == "END" then
-        local stats = getdiff(self.mstart, marker)
-        self.mstart.stats = stats
-        marker.stats = stats
-        self.mend = marker
-      end
-    end,
-    trace = function(self, msg, trace)
-      trace.itern = self.itern
-    end,
-    protobl = function(self, msg, blinfo)
-      blinfo.itern = self.itern
-    end,
-    alltraceflush = function(self, msg, flush)
-      flush.itern = self.itern
-    end,
-    protoloaded = function(self, msg,  address, proto)
-      proto.itern = self.itern
+local actions = mixin.actions
+
+local start_marker, end_marker = "BEGIN", "END"
+
+function actions:stringmarker(msg, marker)
+  if marker.label == start_marker then
+    self.mstart = marker
+    self.mend = nil
+    marker.n = self.itern
+    self.itern = self.itern + 1
+  elseif marker.label == end_marker then
+    local stats = getdiff(self.mstart, marker)
+    self.mstart.stats = stats
+    marker.stats = stats
+    self.mend = marker
+  end
+end
+
+function actions:trace(msg, trace)
+  trace.itern = self.itern
+end
+
+function actions:protobl(msg, blinfo)
+  blinfo.itern = self.itern
+end
+
+function actions:alltraceflush(msg, flush)
+  flush.itern = self.itern
+end
+
+function actions:protoloaded(msg, address, proto)
+  proto.itern = self.itern
+end
+
+function actions:perf_timers(msg)
+  local timerdef = self.enums.timers
+  local times_length = msg:get_times_length()
+  local ids = msg.ids_length ~= 0 and msg:get_ids()
+  local timers = {}
+
+  local tscfreq = self.tscfreq or 3700000000.0
+  local times = msg:get_times()
+  for i=0, times_length-1 do
+    local key
+    if ids then
+      key = timerdef[ids[i]]
+    else
+      key = timerdef[i]
     end
-  }
-}
+    timers[key] = tonumber(times[i])/tscfreq
+  end
+  assert(not self.mstart.timers)
+  self.mstart.timers = timers
+end
+
+function actions:perf_counters(msg)
+  local counterdef = self.enums.counters
+  local counts_length = msg:get_counts_length()
+  local ids = msg.ids_length ~= 0 and msg:get_ids()
+
+  local counters = {}
+  local counts = msg:get_counts()
+  for i=0, counts_length-1 do
+    local key
+    if ids then
+      key = counterdef[ids[i]]
+    else
+      key = counterdef[i]
+    end
+    counters[key] = counts[i]
+  end
+  assert(not self.mstart.counters)
+  self.mstart.counters = counters
+end
+
+function actions:section(msg, id, isstart, length)
+  if not length or not self.mstart then
+    return
+  end
+
+  local marker = self.mstart
+  if not marker.section_times then
+    marker.section_times = {}
+    marker.section_counts = {}
+  end
+  
+  local name = self.section_names[id]
+  marker.section_times[name] = (marker.section_times[name] or 0ull) + length
+  marker.section_counts[name] = (marker.section_counts[name] or 0) + 1
+end
+
 
 return mixin
--- jitlog_actions/make_kruninstr.lua
+++ jitlog_actions/make_kruninstr.lua
@@ -31,10 +31,58 @@ function action.logopened(reader, logpath)
   reader.instr_path = dir .. "/" .. fname
 end
 
+local function allzero(list)
+  for i, v in ipairs(list) do
+    if v ~= 0 then
+      return false, i
+    end
+  end
+  return true
+end
+
+local firstlist = true
+
+local function write_timelist(file, name, values, last)
+  if allzero(values) then
+    return false
+  end
+  if not firstlist then
+    firstlist = false
+    result:write(',\n')
+  end
+  result:write('  { "label" : "', name, '", "data" : [', table.concat(values, ", "))
+  result:write(']}')
+  return true
+end
+
+local function add_ifnonzero(t, label, list)
+  if allzero(list) then
+    return
+  end
+  local entry = {label = label, data = list}
+  table.insert(t, entry)
+  return entry
+end
+
 function action.logparsed(jlog)
   local traces = table.new(jlog.itern, 0)
   local aborts = table.new(jlog.itern, 0)
   local exits = table.new(jlog.itern, 0)
+  local timers, counters
+  
+  if jlog.timers then
+    timers = {}
+    for k, v in pairs(jlog.timers) do
+      timers[k] = table.new(jlog.itern, 0)
+    end
+  end
+
+  if jlog.counters then
+    counters = {}
+    for k, v in pairs(jlog.counters) do
+      counters[k] = table.new(jlog.itern, 0)
+    end
+  end
 
   local mstart
   for i, marker in ipairs(jlog.markers) do
@@ -44,28 +92,56 @@ function action.logparsed(jlog)
       traces[n] = stats.traces
       aborts[n] = stats.aborts
       exits[n] = stats.exits
+      
+      if marker.timers then
+        for k, list in pairs(timers) do
+          list[n] = marker.timers[k] or 0
+        end
+      end
+      if marker.counters then
+        for k, list in pairs(counters) do
+          list[n] = marker.counters[k] or 0
+        end
+      end
     end
   end
 
   local path = os.getenv("instr_results") or jlog.instr_path
   local result = io.open(path, "w")
 
+  local timer_lists = {}
   if json then
-    local data = {timers = {}}
-    local timers = data.timers
-    table.insert(timers, {label = "traces", data = traces})
-    table.insert(timers, {label = "aborts", data = aborts})
-    table.insert(timers, {label = "exits", data = aborts})
+    -- TODO optionally load existing json and append to timers
+    --timer_lists = data.timers
+  end
+  add_ifnonzero(timer_lists, "traces", traces)
+  add_ifnonzero(timer_lists, "aborts", aborts)
+  add_ifnonzero(timer_lists, "exits", exits)
+
+  if timers then
+    for k, list in pairs(timers) do
+      add_ifnonzero(timer_lists, k, list)
+    end
+  end
+  if counters then
+    for k, list in pairs(counters) do
+      add_ifnonzero(timer_lists, k.."_count", list)
+    end
+  end
+  
+  if json then
+    local data = {timers = timer_lists}
     result:write(json.encode(data))
   else
     result:write('{\n  "timers" : [\n')
-    result:write('  { "label" : "traces", "data" : [', table.concat(traces, ", "), ']},\n')
-    result:write('  { "label" : "aborts", "data" : [', table.concat(aborts, ", "), ']},\n')
-    result:write('  { "label" : "exits", "data" : [', table.concat(exits, ", "), ']}\n')
+    for label, list in pairs(timer_lists) do
+      write_timelist(file, label, list)
+    end
     result:write(']}\n')
   end
   result:flush()
   result:close()
 end
+
 
 return action
