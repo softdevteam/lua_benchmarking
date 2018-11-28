6973a8f0f038a284246ac7446f0f67c12fe0d02d
fsfod <fsfod11@gmail.com>
2018-11-28 23:50:25 +0000

    krun run

--- build.sh
+++ build.sh
@@ -55,26 +55,6 @@ function copy_binaries() {
 
 BASE_XCFLAGS=${BASE_XCFLAGS:=""}
 
-#Unmodified build with 32 bit sized GC object pointers. Object allocation limited to the lower 4gb virtual address space
-make -C ${ljsrc} clean
-make -C ${ljsrc} -j XCFLAGS="${BASE_XCFLAGS}"
-copy_binaries "normal"
-
-#Build with JIT removed
-make -C ${ljsrc} clean
-make -C ${ljsrc} -j XCFLAGS="${BASE_XCFLAGS} -DLUAJIT_DISABLE_JIT"
-copy_binaries "nojit"
-
-##GC64 64 bit sized GC object pointer
-make -C ${ljsrc} clean
-make -C ${ljsrc} -j XCFLAGS="${BASE_XCFLAGS} -DLUAJIT_ENABLE_GC64"
-copy_binaries "gc64"
-
-## Build with dual number mode enabled
-make -C ${ljsrc} clean
-make -C ${ljsrc} -j XCFLAGS="${BASE_XCFLAGS} -DLUAJIT_NUMMODE=2"
-copy_binaries "dualnum"
-
 if [ -d "raptorjit_repo" ]; then
   make -C ./raptorjit_repo clean
   make -C ./raptorjit_repo -j HOST_LUA=${ljbins}/normal/luajit
@@ -84,41 +64,3 @@ if [ -d "raptorjit_repo" ]; then
   cp ./raptorjit_repo/src/jit/*.lua ${ljbins}/raptorjit/jit/
 fi
 
-## Optional 32 bit build
-if [ ${BUILD_LJ32} ]; then
-  make -C ${ljsrc} clean
-  make -C ${ljsrc} -j CC="gcc -m32" XCFLAGS="${BASE_XCFLAGS}"
-  copy_binaries "luajit32"
-fi
-
-if [ ${BUILDALL} ]; then
-  if [ ! -d "lua-vermelha" ]; then
-    git clone  --recursive https://github.com/Leonardo2718/lua-vermelha
-  fi
-
-  mkdir -p ${ljbins}/lua-vermelha
-
-  # Make directory path relative so we don't have to worry about what path we have to pass to make.
-  ljbins="$( cd "$ljbins" && pwd )"
-
-  cd lua-vermelha/omr/
-  make -f run_configure.mk SPEC=linux_x86-64 OMRGLUE=./example/glue
-  cd ..
-  make -j
-  cd ..
-  cp lua-vermelha/luav ${ljbins}/lua-vermelha/luav
-fi
-
-# We don't have access to string functions in the config file so pass the base dir for paths
-export LUAVM_BASEDIR=$(pwd)/builds/normal
-export LUAROCKS_CONFIG=$(pwd)/rocks_config.lua
-
-luarocks --tree=rocks install https://raw.githubusercontent.com/fsfod/luachild/master/luachild-0.1-1.rockspec
-
-echo "--------------Running benchmark build scripts-------------------------"
-
-for subdir in benchmarks/*; do
-  if [ -f "$subdir/build.sh" ]; then
-    (cd "$subdir" && ./build.sh)
-  fi
-done
